dd39eacfb9e6d65f5c05ef0b7b82b237355fcbf5
LAPTOP-NDTF2MRT\jiank <cjk7072@closeli.cn>
2023-04-01 22:38:38 +0800

    custom window

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-const { ipcRenderer } = require('electron')
+const { ipcRenderer } = require('electron');
 
 window.addEventListener('DOMContentLoaded', () => {
-  // 点击按钮打开一个新窗口
-  const oBtn = document.getElementById('btn')
-  oBtn.addEventListener('click', () => {
-    ipcRenderer.send('openIndex')
-  })
-})
+    // 显示当前 electron 运行的环境
+    const replaceText = (selector, text) => {
+        const element = document.getElementById(selector);
+        if (element) element.innerText = text;
+    };
+
+    for (const type of ['chrome', 'node', 'electron']) {
+        replaceText(`${type}-version`, process.versions[type]);
+    }
+
+    // 点击按钮打开一个新窗口
+    document.getElementById('btn').addEventListener('click', () => {
+        ipcRenderer.send('openNewWindow');
+    });
+
+    // 获取元素添加点击操作的监听
+    let aBtn = document
+        .getElementsByClassName('windowTool')[0]
+        .getElementsByTagName('div');
+
+    aBtn[0].addEventListener('click', () => {
+        // 当前事件发生后说明需要关闭窗口
+        ipcRenderer.send('window-close'); // 通知主进程我要关闭
+    });
+
+    aBtn[1].addEventListener('click', () => {
+        ipcRenderer.send('window-max'); // 通知主进程我要进行最大化 或 还原
+    });
+
+    aBtn[2].addEventListener('click', () => {
+        ipcRenderer.send('window-min'); // 通知主进程我要进行窗口最小化操作
+    });
+});
--- main.js
+++ main.js
@@ -1,104 +1,130 @@
 // 模块来控制应用程序生命周期和创建本机浏览器窗口
-const {app, BrowserWindow, ipcMain } = require('electron')
-const path = require('path')
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
 
-
+let newWin = null;
+let mainWin = null;
 
 // 创建一个窗口，加载一个界面，界面通过 web 技术实现的，界面运行在渲染进程中
-function createWindow () {
-  // 创建浏览器窗口
-  const mainWin = new BrowserWindow({
-    x: 100,
-    y: 100,  // 设置窗口显示的位置，相对于当前屏幕的左上角
-    show: false,  // 默认情况下创建一个窗口对象之后就会显示，设置为false 就不会显示了
-    width: 800,
-    height: 400,
-    maxHeight: 600,
-    maxWidth: 1000,
-    minHeight: 200,
-    minWidth: 300,  // 可以通过 min max 来设置当前应用窗口的最大和最小尺寸
-    // resizable: false,  // 是否允许缩放应用的窗口大小 
-    // frame: false,  // 设置为 false 时可以创建一个无边框窗口 默认值为 true
-    // transparent: true,  // 是否透明
-    autoHideMenuBar: true,  // 是否显示菜单栏
-    icon: 'lg.ico',  // 设置一个图片路径，可以自定义当前应用的显示图标
-    title: "Hello Electron",  // 自定义当前应用的显示标题
-    webPreferences: {
-      nodeIntegration: true,  // 是否启用Node integration. 默认值为 false.
-      contextIsolation: false,  // Electron 10+ 中，remote 模块默认处于禁用状态。 
-      // enableRemoteModule: true, // Electron 14+  已经废弃
-      // preload: path.join(__dirname, 'preload.js')
-    }
-  })
+function createWindow() {
+    // 创建浏览器窗口
+    mainWin = new BrowserWindow({
+        x: 100,
+        y: 100, // 设置窗口显示的位置，相对于当前屏幕的左上角
+        show: false, // 默认情况下创建一个窗口对象之后就会显示，设置为false 就不会显示了
+        width: 800,
+        height: 400,
+        maxHeight: 600,
+        maxWidth: 1000,
+        minHeight: 200,
+        minWidth: 300, // 可以通过 min max 来设置当前应用窗口的最大和最小尺寸
+        // resizable: false,  // 是否允许缩放应用的窗口大小
+        frame: false, // 设置为 false 时可以创建一个无边框窗口 默认值为 true
+        // transparent: true,  // 是否透明
+        autoHideMenuBar: true, // 是否显示菜单栏
+        icon: 'lg.ico', // 设置一个图片路径，可以自定义当前应用的显示图标
+        title: 'Hello Electron', // 自定义当前应用的显示标题
+        webPreferences: {
+            nodeIntegration: true, // 是否启用Node integration. 默认值为 false.
+            contextIsolation: false, // Electron 10+ 中，remote 模块默认处于禁用状态。
+            // enableRemoteModule: true, // Electron 14+  已经废弃
+            // preload: path.join(__dirname, 'preload.js'),
+        },
+    });
 
-  // 加载应用的 index.html
-  mainWin.loadFile('index.html')
+    // 加载应用的 index.html
+    mainWin.loadFile('index.html');
 
-  mainWin.on('ready-to-show', () => {
-    mainWin.show()
-  })
+    mainWin.on('ready-to-show', () => {
+        mainWin.show();
+    });
 
-  // 打开 DevTools
-  // mainWin.webContents.openDevTools()
+    // 打开 DevTools
+    // mainWin.webContents.openDevTools()
 
-  mainWin.webContents.on('dom-ready', () => {
-    console.log('222 dom-ready...')
-  })
+    mainWin.webContents.on('dom-ready', () => {
+        console.log('222 dom-ready...');
+    });
 
-  mainWin.webContents.on('did-finish-load', () => {
-    console.log('333 did-finish-load...')
-  })
+    mainWin.webContents.on('did-finish-load', () => {
+        console.log('333 did-finish-load...');
+    });
 
-  mainWin.on('close', () => {
-    console.log('888 close...')
-    // mainWin = null
-  })
+    mainWin.on('close', () => {
+        console.log('888 close...');
+        // mainWin = null
+    });
 }
 
 // 当 Electron 完成时，该方法将被调用（app 启动完成）
 // 初始化并准备创建浏览器窗口。
 // 某些api只能在此事件发生后使用。
 app.whenReady().then(() => {
-  createWindow()
-  console.log('111 ready...')
+    createWindow();
+    console.log('111 ready...');
 
-  ipcMain.on('openIndex', () => {
-      // ?? 如何去创建窗口
-      let indexMin = new BrowserWindow({
-        width: 200,
-        height: 200
-      })
-      indexMin.loadFile('list.html')
+    app.on('activate', function () {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
 
-      indexMin.on("close", () => {
-        indexMin = null
-      })
-  })
+ipcMain.on('openNewWindow', () => {
+    if (!newWin) {
+        // ?? 如何去创建窗口
+        let indexMin = new BrowserWindow({
+            width: 200,
+            height: 200,
+        });
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+        indexMin.loadFile('list.html');
+
+        indexMin.on('close', () => {
+            indexMin = null;
+        });
+    }
+});
+
+ipcMain.on('get-current-window', (event) => {
+    const currentWindow = BrowserWindow.getFocusedWindow();
+    event.returnValue = currentWindow;
+});
+
+ipcMain.on('window-min', () => {
+    mainWin.minimize();
+});
+
+ipcMain.on('window-max', () => {
+    if (mainWin.isMaximized()) {
+        // 为true表示窗口已最大化
+        mainWin.restore(); // 将窗口恢复为之前的状态.
+    } else {
+        mainWin.maximize();
+    }
+});
+
+ipcMain.on('window-close', () => {
+    mainWin.close();
+});
 
 // 当所有窗口都关闭时退出
 app.on('window-all-closed', function () {
-  console.log('444 window-all-closed...')
-  if (process.platform !== 'darwin') app.quit()
-})
+    console.log('444 window-all-closed...');
+    if (process.platform !== 'darwin') app.quit();
+});
 
 app.on('before-quit', () => {
-  console.log('555 before-quit...')
-})
+    console.log('555 before-quit...');
+});
 
 app.on('will-quit', () => {
-  console.log('666 will-quit...')
-})
+    console.log('666 will-quit...');
+});
 
 app.on('quit', () => {
-  console.log('777 quit...')
-})
+    console.log('777 quit...');
+});
 
 /* 
   在这个文件中，你可以包含你的应用程序的其他特定的主进程代码。
